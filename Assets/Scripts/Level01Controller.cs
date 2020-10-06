6597280c34c4ed54d19ff71324df9f0d37d75d00
Chase <chasekat742@gmail.com>
2020-10-05 20:17:50 -0500

    High Score saving system
    Score during level, high score on main menu. Saves. Reset Data now sets high score back to 0, Exit quits the application.

--- Assets/Scripts/Level01Controller.cs
+++ Assets/Scripts/Level01Controller.cs
@@ -2,20 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Level01Controller : MonoBehaviour
 {
-    // Update is called once per frame
-    void Update()
+    [SerializeField] Text _currentScoreTextView;
+
+    int _currentScore;
+
+    private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            IncreaseScore(5);
+        }
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             ExitLevel();
         }
     }
 
+    public void IncreaseScore(int scoreIncrease)
+    {
+        _currentScore += scoreIncrease;
+        _currentScoreTextView.text =
+            "> SCORE = " + _currentScore.ToString();
+    }
+
     public void ExitLevel()
     {
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        if (_currentScore > highScore)
+        {
+            PlayerPrefs.SetInt("HighScore", _currentScore);
+            Debug.Log("New High Score: " + _currentScore);
+        }
         SceneManager.LoadScene("MainMenu");
     }
 }
--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] AudioClip _startingSong;
+    [SerializeField] Text _highScoreTextView;
 
     void Start()
     {
+        ScoreDisplay();
+
         if (_startingSong != null)
         {
             AudioManager.Instance.PlaySong(_startingSong);
         }
+    }
+
+    void ScoreDisplay()
+    {
+        int highScore = PlayerPrefs.GetInt("HighScore");
+        _highScoreTextView.text =
+            "> HIGH SCORE = " + highScore.ToString();
+    }
+
+    public void ResetData()
+    {
+        PlayerPrefs.SetInt("HighScore", 0);
+        ScoreDisplay();
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
