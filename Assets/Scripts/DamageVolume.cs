6c3549053fd64cc4bb055fadb8f784b3aa98a602
Chase <chasekat742@gmail.com>
2020-10-06 21:33:49 -0500

    Damage Volume
    Added a damage volume that damages player health by 15 points when entered

---
+++ Assets/Scripts/DamageVolume.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageVolume : MonoBehaviour
+{
+    [SerializeField] CharacterController _player;
+
+    public int damage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == _player)
+        {
+            PlayerHealth.Player.HealthChange(-Mathf.Abs(damage));
+        }
+    }
+}
