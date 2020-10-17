76313fa34c1ccf9c03687da70e330cd1b8980645
Chase <chasekat742@gmail.com>
2020-10-17 17:59:46 -0500

    Added Raycast
    added a raycast to the weapon

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -6,6 +6,12 @@ public class WeaponController : MonoBehaviour
 {
     [SerializeField] ParticleSystem _weaponFlash;
     [SerializeField] AudioClip _weaponFire;
+    [SerializeField] Camera _mainCamera;
+    [SerializeField] Transform _rayOrigin;
+
+    RaycastHit rayHit;
+
+    public float shootDistance = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +26,21 @@ public class WeaponController : MonoBehaviour
         {
             _weaponFlash.Play();
             AudioSource.PlayClipAtPoint(_weaponFire, transform.position);
+            Shoot();
         }
+    }
+
+    void Shoot()
+    {
+        //calculate direction for ray
+        Vector3 rayDirection = _mainCamera.transform.forward;
+        //cast a debug ray
+        Debug.DrawRay(_rayOrigin.position, rayDirection * shootDistance, Color.blue, 1f);
+        //do ray
+        if (Physics.Raycast(_rayOrigin.position, rayDirection, out rayHit, shootDistance))
+            Debug.Log("Ray hit a " + rayHit.transform.gameObject);
+        else
+            Debug.Log("Miss");
+
     }
 }
